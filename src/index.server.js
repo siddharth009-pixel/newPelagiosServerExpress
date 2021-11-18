f316b7be0e72461aa700717162f99b51c62a1907
Siddharth.Patel <opensiddhu993@gmail.com>
2021-11-19 01:29:49 +0530

    mongodb atlas database connected

--- src/index.server.js
+++ src/index.server.js
@@ -1,49 +1,50 @@
-const express=require('express')
-const app=express()
-const _env=require('dotenv')
-const bodyParser=require('body-parser')
-const mongoose=require('mongoose')
-const userRoutes=require('./routes/user')
-const adminRoutes=require('./routes/admin/user')
-const categoryRoutes=require('./routes/category')
-const productRoutes=require('./routes/product')
-const cartRoutes=require('./routes/cart')
-const initialDataRoutes=require('./routes/admin/initialData')
-const pageRoutes=require('./routes/admin/page')
-const addressRoutes=require('./routes/address')
-const orderRoutes=require('./routes/order')
-const adminOrderRoutes=require('./routes/admin/order')
-const reviewRoutes=require('./routes/review')
+const express = require('express')
+const app = express()
+const _env = require('dotenv')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const userRoutes = require('./routes/user')
+const adminRoutes = require('./routes/admin/user')
+const categoryRoutes = require('./routes/category')
+const productRoutes = require('./routes/product')
+const cartRoutes = require('./routes/cart')
+const initialDataRoutes = require('./routes/admin/initialData')
+const pageRoutes = require('./routes/admin/page')
+const addressRoutes = require('./routes/address')
+const orderRoutes = require('./routes/order')
+const adminOrderRoutes = require('./routes/admin/order')
+const reviewRoutes = require('./routes/review')
 var jwt = require('express-jwt');
 var jwks = require('jwks-rsa');
 // const { requiredSignIn } = require('./common-middleware')
-const path=require('path')
-const cors=require('cors')
+const path = require('path')
+const cors = require('cors')
 _env.config();
 
 var port = process.env.PORT || 8080;
 
 var jwtCheck = jwt({
-      secret: jwks.expressJwtSecret({
-          cache: true,
-          rateLimit: true,
-          jwksRequestsPerMinute: 5,
-          jwksUri: 'https://pelagios.au.auth0.com/.well-known/jwks.json'
-    }),
-    audience: 'pelagiosart.com',
-    issuer: 'https://pelagios.au.auth0.com/',
-    algorithms: ['RS256']
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: 'https://pelagios.au.auth0.com/.well-known/jwks.json'
+  }),
+  audience: 'pelagiosart.com',
+  issuer: 'https://pelagios.au.auth0.com/',
+  algorithms: ['RS256']
 }).unless(['/api/check']);
 
 
 mongoose
   .connect(
-    `mongodb://localhost:27017/onemart`,
+    `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@cluster0.yeyo4.mongodb.net/${process.env.DATABASE_NAME}?retryWrites=true&w=majority`
+    ,
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
-      useFindAndModify:false
+      useFindAndModify: false
     }
   )
   .then(() => {
@@ -53,34 +54,35 @@ mongoose
     console.log(err);
   });
 
-//mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@cluster0.yeyo4.mongodb.net/${process.env.DATABASE_NAME}?retryWrites=true&w=majority
+// `mongodb://localhost:27017/onemart`
+// mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@cluster0.yeyo4.mongodb.net/${process.env.DATABASE_NAME}?retryWrites=true&w=majority
 
 
 app.use(cors())
 // app.use(jwtCheck);
 app.use(express.json())
-app.use('/public',express.static(path.join(__dirname,'uploads')))
-app.use('/api',initialDataRoutes);
-app.use('/api',userRoutes);
-app.use('/api',adminRoutes)
-app.use('/api',categoryRoutes)
-app.use('/api',productRoutes)
-app.use('/api',cartRoutes)
-app.use('/api',pageRoutes)
-app.use('/api',addressRoutes)
-app.use('/api',orderRoutes)
-app.use('/api',adminOrderRoutes)
-app.use('/api',reviewRoutes)
+app.use('/public', express.static(path.join(__dirname, 'uploads')))
+app.use('/api', initialDataRoutes);
+app.use('/api', userRoutes);
+app.use('/api', adminRoutes)
+app.use('/api', categoryRoutes)
+app.use('/api', productRoutes)
+app.use('/api', cartRoutes)
+app.use('/api', pageRoutes)
+app.use('/api', addressRoutes)
+app.use('/api', orderRoutes)
+app.use('/api', adminOrderRoutes)
+app.use('/api', reviewRoutes)
 
 
-app.get('/api/check',(req,res)=>{
-    
-    console.log("you are signed in")
-    res.send("you are logged in")
+app.get('/api/check', (req, res) => {
+
+  console.log("you are signed in")
+  res.send("you are logged in")
 })
 
 
-app.listen(2000,()=>{
-    console.log(`server running pn port number http://localhost:2000`)
+app.listen(2000, () => {
+  console.log(`server running pn port number http://localhost:2000`)
 })
 
